3393cf117d54aa82b5a189729141c5fceab2bb2e
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-04-12 17:37:37 +0000

    Square with private size attribute

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module defines a class Square with a private size attribute."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
