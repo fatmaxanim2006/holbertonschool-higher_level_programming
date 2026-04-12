26d1511d057827f9c27bfb8f626abbfcbccff80c
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-04-12 17:55:19 +0000

    Square with getter and setter

---
+++ python-classes/4-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""This module defines a class Square with getter and setter."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Get the current size of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square with validation.
+
+        Args:
+            value (int): The new size.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current square area."""
+        return (self.__size * self.__size)
