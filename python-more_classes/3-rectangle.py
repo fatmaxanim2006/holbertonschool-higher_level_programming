ccd9b6c863ff91f5fd5fa2473a2c8ad97d448c49
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-04-13 14:40:52 +0000

    Clean version of task 3

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
-"""
-Rectangle class-ı üçün modul.
-"""
+"""Module for Rectangle class."""
 
 
 class Rectangle:
-    """Düzbucaqlını təyin edən class."""
+    """Defines a rectangle."""
 
     def __init__(self, width=0, height=0):
-        """İlkin dəyərlər."""
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """Eni qaytarır."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Eni təyin edir."""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -28,12 +23,10 @@ class Rectangle:
 
     @property
     def height(self):
-        """Hündürlüyü qaytarır."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Hündürlüyü təyin edir."""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -41,21 +34,17 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """Sahəni hesablayır."""
         return self.__width * self.__height
 
     def perimeter(self):
-        """Perimetri hesablayır."""
         if self.__width == 0 or self.__height == 0:
             return 0
         return (self.__width * 2) + (self.__height * 2)
 
     def __str__(self):
-        """Düzbucaqlını # ilə çəkir."""
         if self.__width == 0 or self.__height == 0:
             return ""
         return (("#" * self.__width + "\n") * self.__height)[:-1]
 
     def __repr__(self):
-        """Obyektin string təmsilini qaytarır."""
         return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
