748dc78af7d68bba42c27d49ef31a2315d65a9c0
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-04-13 17:24:21 +0000

    Add lookup function

--- 0-lookup.py
+++ 0-lookup.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
+"""
+Bu modul obyektlərin atributlarını axtarmaq üçün funksiyanı ehtiva edir.
+"""
+
 
 def lookup(obj):
+    """
+    Obyektin mövcud atribut və metodlarının siyahısını qaytarır.
+
+    Args:
+        obj: Yoxlanılacaq obyekt.
+
+    Returns:
+        list: Atributların adlarından ibarət siyahı.
+    """
     return dir(obj)
