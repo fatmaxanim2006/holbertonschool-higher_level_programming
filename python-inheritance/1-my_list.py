66adb36f68f1aa113a332209affcb5da05fef48f
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-04-13 19:43:13 +0000

    Add my_list in correct directory

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-"""Module for MyList class"""
+"""
+Module for MyList class
+"""
 
 
 class MyList(list):
-    """A class MyList that inherits from list"""
+    """
+    MyList class inherits from list
+    """
 
     def print_sorted(self):
-        """Prints the list, but sorted in ascending order"""
+        """
+        Prints the list in ascending order
+        """
         print(sorted(self))
