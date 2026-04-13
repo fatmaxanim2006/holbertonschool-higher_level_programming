94d78df07cce4b29e574be9819a240d165dfc304
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-04-13 19:12:26 +0000

    Fix Tasks 1-4 and Add Task 5: Corrected docstrings and inheritance logic

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -3,5 +3,5 @@
 
 
 def is_same_class(obj, a_class):
-    """Checks if an object is exactly an instance of a specified class"""
+    """Checks if an object is exactly an instance of a class"""
     return type(obj) is a_class
--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -3,5 +3,5 @@
 
 
 def is_kind_of_class(obj, a_class):
-    """Checks if an object is an instance of, or inherited from, a class"""
+    """Checks if an object is instance of, or inherited from, a class"""
     return isinstance(obj, a_class)
