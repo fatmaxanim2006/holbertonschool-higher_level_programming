38c0b2359e02f29f379ed6e7cc2bc5475b92eab3
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-04-13 19:52:01 +0000

    Add inherits_from function

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""Module for inherits_from function"""
+"""Module for inherits_from method"""
 
 
 def inherits_from(obj, a_class):
-    """Returns True if object is an instance of a class that inherited from a class"""
+    """Checks if object is an instance of a class that inherited
+    from the specified class
+    """
     return issubclass(type(obj), a_class) and type(obj) is not a_class
