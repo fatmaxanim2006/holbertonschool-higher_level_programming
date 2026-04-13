f52bb541a202610a6f8f6b76ea9553109ca4ca6f
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-04-13 14:21:46 +0000

    Add 0-rectangle.py and README.md

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Bu modul boş bir Rectangle class-ı təyin edir.
+"""
+
+
+class Rectangle:
+    """Boş bir düzbucaqlı class-ı."""
+    pass
---
+++ python-more_classes/README.md
@@ -0,0 +1,1 @@
+Project: Python - More Classes and Objects
