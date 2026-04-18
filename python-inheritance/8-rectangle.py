919e1ff105abe7906a03d704079e5c3015781966
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-04-18 09:39:55 +0000

    Fix doctests for Task 7

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python3
-"""Module for Rectangle class"""
-BaseGeometry = __import__("7-base_geometry").BaseGeometry
+"""Rectangle sinfi üçün modul"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """A class Rectangle that inherits from BaseGeometry"""
+    """BaseGeometry-dən miras alan Rectangle sinfi"""
 
     def __init__(self, width, height):
-        """Initializes width and height after validation"""
+        """En və hündürlüyü təyin edir və doğrulayır"""
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
-
-    def area(self):
-        """Returns the area of the rectangle"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """Returns the rectangle description"""
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
