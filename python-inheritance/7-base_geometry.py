a1f32e37c3d0608cdd5e0b601d5aa64db91dfdad
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-04-18 10:41:44 +0000

    Add required doctests and fix validator

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -6,7 +6,7 @@ class BaseGeometry:
     """Represent base geometry."""
 
     def area(self):
-        """Not yet implemented."""
+        """Not implemented."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
