c6402a5fc2f37dd8bbbc8ab3879fd76ef64efdaa
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-04-18 17:03:12 +0000

    Implement FlyingFish with multiple inheritance

---
+++ python-abc/python-abc/main_04_flyingfish.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from task_04_flyingfish import Fish, FlyingFish
+
+flying_fish = FlyingFish()
+flying_fish.swim()
+flying_fish.fly()
+flying_fish.habitat()
+
+# MRO-nu yoxlamaq üçün (İstəyə bağlı)
+# print(FlyingFish.mro())
