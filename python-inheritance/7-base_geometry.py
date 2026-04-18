a57eabf2bb4d0c9bbf40c8c7f3e54e9b9902242c
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-04-18 09:43:13 +0000

    Final fix for task 7 doctests

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,17 +1,5 @@
-#!/usr/bin/python3
-"""BaseGeometry sinfi üçün modul"""
-
-
-class BaseGeometry:
-    """Həndəsə əməliyyatları üçün baza sinfi"""
-
-    def area(self):
-        """Hələ tətbiq olunmayıbsa Exception qaytarır"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Dəyərin tam ədəd və 0-dan böyük olmasını yoxlayır"""
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+def integer_validator(self, name, value):
+    if type(value) is not int:
+        raise TypeError("{} must be an integer".format(name))
+    if value <= 0:
+        raise ValueError("{} must be greater than 0".format(name))
