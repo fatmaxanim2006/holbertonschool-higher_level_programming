8c5c15a425cef6a47d53bd80e63b5de82f9531c2
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-04-18 12:06:12 +0000

    Fixing type validation and PEP8

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
-"""BaseGeometry modulu üçün sənədləşmə."""
+"""Defines a base geometry class BaseGeometry."""
 
 
 class BaseGeometry:
-    """BaseGeometry sinfi."""
+    """Represent base geometry."""
 
     def area(self):
-        """Sahəni hesablayan metod (hələ icra olunmayıb)."""
+        """Not implemented."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Dəyərin tam ədəd və 0-dan böyük olmasını yoxlayır.
+        """Validate a parameter as an integer.
 
         Args:
-            name (str): Parametrin adı.
-            value (int): Yoxlanılacaq dəyər.
-
+            name (str): The name of the parameter.
+            value (int): The parameter to validate.
         Raises:
-            TypeError: Əgər value int deyilsə.
-            ValueError: Əgər value <= 0 olarsa.
+            TypeError: If value is not an integer.
+            ValueError: If value is <= 0.
         """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-
