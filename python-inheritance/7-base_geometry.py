d58168247cc69fc6eab7672109d3a3e29c2cc6aa
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-04-18 12:02:06 +0000

    Integer validator implemented

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
-"""Defines a base geometry class BaseGeometry."""
+"""BaseGeometry modulu üçün sənədləşmə."""
 
 
 class BaseGeometry:
-    """Represent base geometry."""
+    """BaseGeometry sinfi."""
 
     def area(self):
-        """Not implemented."""
+        """Sahəni hesablayan metod (hələ icra olunmayıb)."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validate a parameter as an integer.
+        """Dəyərin tam ədəd və 0-dan böyük olmasını yoxlayır.
 
         Args:
-            name (str): The name of the parameter.
-            value (int): The parameter to validate.
+            name (str): Parametrin adı.
+            value (int): Yoxlanılacaq dəyər.
+
         Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is <= 0.
+            TypeError: Əgər value int deyilsə.
+            ValueError: Əgər value <= 0 olarsa.
         """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
